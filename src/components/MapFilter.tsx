26eccf1eea409bb653bd0fcd09ddfaf4dc89d7a3
lvinko <Vadym.Shevchenko@lnu.edu.ua>
2024-11-21 22:37:07 +0200

    feat: filters localization

--- src/components/MapFilter.tsx
+++ src/components/MapFilter.tsx
@@ -3,30 +3,55 @@ import { useMapFilter } from "@/context/MapFilterContext";
 const MapFilter = () => {
   const { filters, setFilters } = useMapFilter();
 
-  return (
-    <div className="flex gap-4 items-center absolute bottom-24 right-8 z-10 text-zinc-900">
-      <select
-        value={filters.year}
-        onChange={(e) => setFilters(prev => ({...prev, year: parseInt(e.target.value)}))}
-        className="px-4 py-2 border rounded-md"
-      >
-        {Array.from({length: 20}, (_, i) => 2003 + i).map(year => (
-          <option key={year} value={year}>
-            {year}
-          </option>
-        ))}
-      </select>
+  const fitlerLabelMap = {
+    year: {
+      label: 'Рік',
+      options: Array.from({ length: 20 }, (_, i) => 2003 + i).map(year => ({
+        value: year,
+        label: year.toString()
+      }))
+    },
+    type: {
+      label: 'Тип населенного пункту',
+      options: [
+        { value: 'urban', label: 'Міста' },
+        { value: 'rural', label: 'Селища' },
+        { value: 'total', label: 'Всі населені пункти' }
+      ]
+    }
+  }
 
-      <select 
-        value={filters.type}
-        onChange={(e) => setFilters(prev => ({...prev, type: e.target.value}))}
-        className="px-4 py-2 border rounded-md"
-      >
-        <option value="">Select type</option>
-        <option value="urban">Urban</option>
-        <option value="rural">Rural</option>
-        <option value="total">Total</option>
-      </select>
+  return (
+    <div className="flex gap-4 items-center absolute bottom-36 right-8 z-10 text-zinc-900 bg-white p-3 rounded-md">
+      <label className="text-sm font-medium">{fitlerLabelMap.year.label}
+        <select
+          value={filters.year}
+          onChange={(e) => setFilters(prev => ({ ...prev, year: parseInt(e.target.value) }))}
+          className="px-4 py-2 border rounded-md ml-2"
+        >
+          {fitlerLabelMap.year.options.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </label>
+      <div className="h-px bg-black w-px h-4" />
+
+      <label className="text-sm font-medium">{fitlerLabelMap.type.label}
+        <select
+          value={filters.type}
+          onChange={(e) => setFilters(prev => ({ ...prev, type: e.target.value }))}
+          className="px-4 py-2 border rounded-md ml-2"
+        >
+          {fitlerLabelMap.type.options.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </label>
+
     </div>
   );
 };
