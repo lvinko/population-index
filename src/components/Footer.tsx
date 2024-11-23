f6ea5bfd806c6f82e76f58ed3867d2fab3acc85c
lvinko <Vadym.Shevchenko@lnu.edu.ua>
2024-11-23 13:32:49 +0200

    feat: footer copy right and additional links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,8 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="h-12 mt-5 flex items-center justify-between px-4 gap-2">
+    <div className="h-12 mt-5 flex items-center justify-center px-4 gap-2 text-sm">
+      <Link href="/" className="mr-5 text-gray-600 hover:text-gray-900">Головна</Link>
       <Link href="/stats" className="mr-5 text-gray-600 hover:text-gray-900">Статистика</Link>
 
       <div className="w-0.5 h-full bg-gray-600 rounded-full"></div>
@@ -13,7 +14,7 @@ const Footer = () => {
           <Link href="https://stat.gov.ua/uk" target="_blank">
             <Image src="/logo-data-bank.png" alt="Держстат" width={60} height={20} />
           </Link>
-          <Link href="https://dia.gov.ua/" target="_blank">
+          <Link href="https://dia.gov.ua/" target="_blank"> 
             <Image src="/logo-dia.png" alt="Дія" width={30} height={30} />
           </Link>
         </div>
@@ -26,6 +27,10 @@ const Footer = () => {
       <div className="w-0.5 h-full bg-gray-600 rounded-full"></div>
 
       <Link href="/about" className="ml-5 text-gray-600 hover:text-gray-900">Про проект</Link>
+
+      <small className="text-xs text-gray-600">
+        © {new Date().getFullYear()}
+      </small>
     </div>
   );
 };
